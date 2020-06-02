5ea4e4316444e5bf7006b438c99bb5e16e26dedf
Kewi01001 <55188217+Kewi01001@users.noreply.github.com>
2020-06-02 21:08:42 +0200

    Koden till CSS och HTML

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public static void main(String[] args){
+        // Good
+        int minutesPerHour = 60;
+        System.out.printIn(minutesPerHour);
+    }
+}
+
+public class WordPlay {
+    public static void main(string[] args) {
+        int 
+
+    }
+}
+
+
+
